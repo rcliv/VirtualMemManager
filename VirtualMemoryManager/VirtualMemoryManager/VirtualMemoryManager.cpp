48f318b6ed1224829f28b42e9cc4a03371dc5872
rcliv <robbylagen@icloud.com>
2016-12-02 16:22:05 -0600

    Added command line interface and changed page table design for init
    I had to change the page table design due to initialization problems.

--- VirtualMemoryManager/VirtualMemoryManager/VirtualMemoryManager.cpp
+++ VirtualMemoryManager/VirtualMemoryManager/VirtualMemoryManager.cpp
@@ -10,8 +10,8 @@
 using namespace std;
 /* offset is used to address the location within a page */
 #define OFFSET_BITS 8
-/* mask is used to extract the offset from a logical address */ 
-#define OFFSET_MASK 0xFF 
+/* mask is used to extract the offset from a logical address */
+#define OFFSET_MASK 0xFF
 
 #define NUM_PAGES 256
 #define NUM_FRAMES 256
@@ -30,35 +30,31 @@ typedef unsigned int offset_t;
 typedef unsigned int laddress_t;
 typedef unsigned int paddress_t;
 
-/* 
- * In C language, there is no binary format in printf 
- * You must implement the following functions to print binary format 
+/*
+ * In C language, there is no binary format in printf
+ * You must implement the following functions to print binary format
  * itob16() and itob8() are modified from itob() by Xiao Qin.
  */
 char *itob(int x);
 char *itob16(int x);
 char *itob8(int x);
 
-
+// Flags used to determine user's choice
+bool displayAddr;
+bool FIFO;
 
 typedef struct {
-	page_t pageNum;
-	frame_t frameNum;
-	bool valid;
+    page_t pageNum;
+    frame_t frameNum;
+    bool valid;
 } tlbEntry;
 
 typedef struct {
-	tlbEntry tlb_entry[TLB_SIZE];
-	unsigned int next_tlb_ptr;
+    tlbEntry tlb_entry[TLB_SIZE];
+    unsigned int next_tlb_ptr;
 } tlb;
 
-typedef struct {
-	frame_t page[PAGE_SIZE];
-} page;
-
-typedef struct {
-	page page_table[NUM_PAGES];
-} pageTable;
+typedef frame_t page_table[NUM_PAGES];
 
 //Functions
 int logicAdrrLoader(char * fileName, int * logicAddrList);
@@ -66,35 +62,74 @@ int extractLogicAddr(laddress_t address, page_t * pageNum, offset_t * offset);
 int searchTLB(page_t * pageNum, bool * isTlbHit, frame_t * frameNum);
 int searchPageTable(bool * isTlbHit, page_t pageNum, bool * isPageFault, frame_t * frameNum);
 // NEEDS TO ALSO PASS IN PHYSICAL MEMORY BUT I DONT KNOW WHAT THAT MEANS
-int handlePageFault(page_t pageNum, pageTable * pagetable, tlb * tlbUsed);
+int handlePageFault(page_t pageNum, page_table * pagetable, tlb * tlbUsed);
 
 int main()
 {
-	ifstream myfile("Text.txt");
-	int32_t a, masked;
-	int32_t mask;
-	mask = (1 << 16) - 1;
-	
-	while (myfile >> a) {
-		printf("\n %d", a);
-		masked = a & mask;
-		printf("\nMasked: %d", masked);
-	}
-
-	myfile.close();
+    cout << "Welcome to Group 1's VM Simulator Version 1.0\n\n"
+    << "Number of logical pages: " << NUM_PAGES << endl
+    << "Page size: " << PAGE_SIZE << " bytes" << endl
+    << "Page table size: " << NUM_FRAMES << endl
+    << "TLB size: " << TLB_SIZE << endl
+    << "Number of physical frames: " << NUM_FRAMES << endl
+    << "Physical memory size: 65,536 bytes\n\n";
+    
+    string displayAddrChoice;
+    
+    cout << "Display physical addresses? [y or n] ";
+    cin >> displayAddrChoice;
+    if (displayAddrChoice == "y" || displayAddrChoice == "Y") {
+        displayAddr = true;
+    }
+    else {
+        displayAddr = false;
+    }
+    
+    int strategyChoice;
+    
+    cout << "Choose TLB Replacement Strategy [1: FIFO, 2: LRU] ";
+    cin >> strategyChoice;
+    if (strategyChoice == 1) {
+        FIFO = true;
+    }
+    else {
+        FIFO = false;
+        
+    }
+    
+    
+    
+    ifstream myfile("Text.txt");
+    int32_t a, masked;
+    int32_t mask;
+    mask = (1 << 16) - 1;
+    
+    while (myfile >> a) {
+        printf("\n %d", a);
+        masked = a & mask;
+        printf("\nMasked: %d", masked);
+    }
+    
+    myfile.close();
     return 0;
 }
 
 int logicAdrrLoader(char * fileName, laddress_t * logicAddrList) {
-
-	return 0;
-
+    
+    return 0;
+    
 }
 int extractLogicAddr(laddress_t address, page_t * pageNum, offset_t * offset) {
-	return 0;
+    return 0;
 }
 
-
+// Page Table Initialization
+int init_page_table(page_table pageTable){
+    for (int i = 0; i < NUM_PAGES; i++) {
+        pageTable[i] = NULL;
+    }
+    return 0;
+}
 
 // TLB functions
 int TLB_init(tlb *tlb) {
@@ -104,13 +139,11 @@ int TLB_init(tlb *tlb) {
     return 0;
 }
 
-int TLB_display(tlb_t tlb) {
-    unsigned int i;
-    
-    for (i = 0; i < TLB_SIZE; i++) {
-        printf("TLB entry " + i + ", page num: " + tlb.tlb_entry[i].pageNum 
-        	+ ", frame num: " + tlb.tlb_entry[i].frameNum);
-        if (tlb.tlb_entry[i].valid == FALSE)
+int TLB_display(tlb tlb) {
+    for (int i = 0; i < TLB_SIZE; i++) {
+        cout << "TLB entry " << i << ", page num: " << tlb.tlb_entry[i].pageNum
+             << ", frame num: " << tlb.tlb_entry[i].frameNum;
+        if (tlb.tlb_entry[i].valid == false)
             printf("Invalid\n");
         else printf("Valide\n");
     }
